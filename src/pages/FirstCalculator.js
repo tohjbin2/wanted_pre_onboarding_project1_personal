47b4e3efb21bbaa471428df1b9562e4208fd9b62
tohjbin <tohjbin@gmail.com>
2022-01-26 23:13:31 +0900

    Add: 첫번째 계산기 레이아웃 구현중

--- src/pages/FirstCalculator.js
+++ src/pages/FirstCalculator.js
@@ -2,7 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 
 function FirstCalculator() {
-  return <div>FirstCalculator</div>;
+  return (
+    <FirstCalPage>
+      <FirstCal />
+    </FirstCalPage>
+  );
 }
 
 export default FirstCalculator;
+
+const FirstCalPage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`;
+
+const FirstCal = styled.div``;
